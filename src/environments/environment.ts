8e78a1417ecf96aa35dc1822308ed63675d0268e
yhbharani <yash.h.bharani@gmail.com>
2020-07-13 17:38:45 +0530

    all iss weellll

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBOfdDD2hIMbdm945LEtxbvmpU-cFKLVqY",
+    authDomain: "chcr-26ac8.firebaseapp.com",
+    databaseURL: "https://chcr-26ac8.firebaseio.com",
+    projectId: "chcr-26ac8",
+    storageBucket: "chcr-26ac8.appspot.com",
+    messagingSenderId: "430423642269",
+    appId: "1:430423642269:web:7b9d4d262f569e1e1a0474"
+  }
 };
 
 /*
