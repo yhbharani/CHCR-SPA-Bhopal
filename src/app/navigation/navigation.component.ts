322d3e3ac96efb328faa85048246f46ef340fefd
yhbharani <yash.h.bharani@gmail.com>
2020-07-18 13:00:58 +0530

    kuch kuch kiya se

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  animations:[
+    trigger('pull',[
+      transition('void => *',[
+        style({ transform: 'translateX(130%)' }),
+        animate('0.4s ease-in')
+      ]),
+      transition('* => void',[
+        animate('0.4s ease-out', style({ transform: 'translateX(130%)' }))
+      ])
+    ])
+  ] 
 })
 export class NavigationComponent implements OnInit {
+
+  drop: boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(){
+    this.drop=!this.drop;
   }
 
 }
