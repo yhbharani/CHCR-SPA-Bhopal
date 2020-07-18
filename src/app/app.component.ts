bb118526f6eb9f424eb76c79c1725446e4b5d07a
yhbharani <yash.h.bharani@gmail.com>
2020-07-18 18:50:20 +0530

    have to do smthing about x-overflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var $:any;
 
 @Component({
   selector: 'app-root',
@@ -10,29 +9,7 @@ declare var $:any;
 export class AppComponent implements OnInit {
   title = 'chcr';
 
-  ngOnInit() :void {
-
-    var scrollLink = $('.scroll');
-  
- 
-    // Active link switching  
-
-    $(window).scroll(function() {
-      
-      var scrollbarLocation = $(this).scrollTop();
-      scrollLink.each(function() {
-    
-        var sectionOffset = $(this.hash).offset().top-10;
-        if ( sectionOffset <= scrollbarLocation ) {
-          $(this).parent().addClass('active');
-          $(this).parent().siblings().removeClass('active');
-        }
-      
-      })
-  
-    })
+  ngOnInit() :void {}
    
-  
-  }
 
 }
