9de3891be1bfb7a6595b3e26f17ff4c4b46eeed8
yhbharani <yash.h.bharani@gmail.com>
2020-07-18 19:47:03 +0530

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBOfdDD2hIMbdm945LEtxbvmpU-cFKLVqY",
+    authDomain: "chcr-26ac8.firebaseapp.com",
+    databaseURL: "https://chcr-26ac8.firebaseio.com",
+    projectId: "chcr-26ac8",
+    storageBucket: "chcr-26ac8.appspot.com",
+    messagingSenderId: "430423642269",
+    appId: "1:430423642269:web:7b9d4d262f569e1e1a0474"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyBOfdDD2hIMbdm945LEtxbvmpU-cFKLVqY",
     authDomain: "chcr-26ac8.firebaseapp.com",
